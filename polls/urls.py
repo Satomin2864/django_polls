547bec5413e42d90068f1c8c6840ff11dacaff19
Satomin2864 <satomiyuta0602@gmail.com>
2017-09-01 00:14:11 +0900

    urlのパスについてコメントを追加

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # /polls/
     url(r'^$', views.index, name='index'),
+    # /polls/数値ならなんでも/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # /polls/数値ならなんでも/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # /polls/数値ならなんでも/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
