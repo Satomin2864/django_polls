612806a5325d7e9de219bcb12bbd0223bfc997e8
Satomin2864 <satomiyuta0602@gmail.com>
2017-09-02 22:19:56 +0900

    404エラーの送信をできるようにする

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
-from .models import Question
+from django.http import Http404
+
+
+from .models import Question 
 
 
 def index(request):
@@ -21,3 +24,11 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+# 404のエラー処理
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'polls/detail.html', {'question': question})
