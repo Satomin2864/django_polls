cbe24884e8e9e2660bbe4b8b58255a85d555953b
Satomin2864 <satomiyuta0602@gmail.com>
2017-09-03 00:36:38 +0900

    URL名の名前空間の設定に書き換え

--- polls/urls.py
+++ polls/urls.py
@@ -2,13 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    # /polls/
     url(r'^$', views.index, name='index'),
-    # /polls/数値ならなんでも/
     url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    # /polls/数値ならなんでも/results/
     url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
-    # /polls/数値ならなんでも/vote/
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
